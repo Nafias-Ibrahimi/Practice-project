cf7c87ce9ef5863e2f29ae04cf292426def9d10d
Nafisa Ebrahimi <nafisai2750@gmail.com>
2026-02-23 16:38:10 +0430

    updated it

---
+++ home.js
@@ -0,0 +1,14 @@
+// const contanier = document.getElementsByClassName('container')[1];
+// contanier.innerHTML = "hello world";
+
+// const container=document.getElementById('container');
+// container.innerHTML="Hello";
+
+// const container=document.querySelector('.container').textContent="Hello second";
+const container=document.querySelector('#container');
+
+let divs="";
+for(let i=1; i<=4; i++){
+    divs +=`<div class="box">box ${i}</div>`;
+}
+container.innerHTML=divs;
