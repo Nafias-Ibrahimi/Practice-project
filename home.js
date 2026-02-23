2810e8a0d9c09ed0766ead453e20167d31bf3888
Nafisa Ebrahimi <nafisai2750@gmail.com>
2026-02-23 20:20:28 +0430

    commit

--- home.js
+++ home.js
@@ -1,29 +1,4 @@
-// const contanier = document.getElementsByClassName('container')[1];
-// contanier.innerHTML = "hello world";
 
-// const container=document.getElementById('container');
-// container.innerHTML="Hello";
 
-// const container=document.querySelector('.container').textContent="Hello second";
-const container=document.querySelector('#container');
 
-// let divs="";
-// for(let i=1; i<=4; i++){
-//     divs +=`<div class="box">box ${i}</div>`;
-// }
-// container.innerHTML=divs;
 
-// for(let i=1; i<=4; i++){
-//     const div=document.createElement('div');
-//     div.className="box";
-//     div.textContent=`box ${i}`;
-//     container.appendChild(div);
-// }
-
-for(let i=1; i<=4; i++){
-    const div=document.createElement('div');
-    div.className='box';
-    div.textContent=`Box ${i}`;
-    div.style.backgroundColor=`var(--color-2)`;
-    container.appendChild(div);
-}
