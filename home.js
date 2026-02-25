7d42310150d02957ed3f09803242f4ba01420f59
Nafisa Ebrahimi <nafisai2750@gmail.com>
2026-02-25 20:39:09 +0430

    add form

--- home.js
+++ home.js
@@ -1,29 +1,39 @@
-const nav=document.querySelector('.nav');
-const brand=document.querySelector('.brand');
+// const nav=document.querySelector('.nav');
+// const brand=document.querySelector('.brand');
 
-let img=document.createElement('img');
-img.src='/images/image.png';
-img.alt="logo";
-brand.style.background='var(--color-4)';
-brand.appendChild(img);
+// let img=document.createElement('img');
+// img.src='/images/image.png';
+// img.alt="logo";
+// brand.style.background='var(--color-4)';
+// brand.appendChild(img);
 
-let links=[
-    {text:"Home" , url:"/home"},
-    {text:"About" , url:"/about"},
-    {text:"Contact" , url:"/contact"},
-]
- for(let i=0; i<=links.length; i++){
-    let li=document.createElement('li');
-    let a=document.createElement('a');
-    li.className="nav-list";
-    a.className="nav-item";
+// let links=[
+//     {text:"Home" , url:"/home"},
+//     {text:"About" , url:"/about"},
+//     {text:"Contact" , url:"/contact"},
+// ]
+//  for(let i=0; i<=links.length; i++){
+//     let li=document.createElement('li');
+//     let a=document.createElement('a');
+//     li.className="nav-list";
+//     a.className="nav-item";
 
    
-    a.textContent=links[i].text;
-    a.href=links[i].url;
-     li.appendChild(a);
-    nav.appendChild(li);
- }
+//     a.textContent=links[i].text;
+//     a.href=links[i].url;
+//      li.appendChild(a);
+//     nav.appendChild(li);
+//  }
 
+const form = document.querySelector('#signup');
 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+
+    console.log(username);
+    console.log(email);
+});
 
