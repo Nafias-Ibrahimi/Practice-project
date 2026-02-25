e42e93fa095896c0cf7145b521c5f03576f331e4
Nafisa Ebrahimi <nafisai2750@gmail.com>
2026-02-25 22:23:14 +0430

    add icon

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,1 +1,18 @@
-!
+const addTask=document.querySelector('#addTask');
+const taskInput=document.querySelector('#taskInput');
+const taskList=document.querySelector('#taskList');
+
+addTask.addEventListener('click' ,()=>{
+    const taskText=taskInput.value.trim();
+    if(!taskText) return alert('please Enter task');
+
+    const li=document.createElement('li');
+    li.innerHTML=`
+    <span class="task-text">${taskText}</span>
+    <button class="delete-btn"><i class="fa-solid fa-trash" style="color: rgb(87, 144, 172);"></i></button>
+    `;
+    taskList.appendChild(li);
+    taskInput.value="";
+
+
+})
