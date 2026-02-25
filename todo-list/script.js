6b6df3ed502cac1536a66c73e843d9e0aeb1102d
Nafisa Ebrahimi <nafisai2750@gmail.com>
2026-02-25 23:07:08 +0430

    have some changes code

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,18 +1,63 @@
-const addTask=document.querySelector('#addTask');
-const taskInput=document.querySelector('#taskInput');
-const taskList=document.querySelector('#taskList');
+// const addTask=document.querySelector('#addTask');
+// const taskInput=document.querySelector('#taskInput');
+// const taskList=document.querySelector('#taskList');
 
-addTask.addEventListener('click' ,()=>{
-    const taskText=taskInput.value.trim();
-    if(!taskText) return alert('please Enter task');
+// addTask.addEventListener('click' ,()=>{
+//     const taskText=taskInput.value.trim();
+//     if(!taskText) return alert('please Enter task');
 
-    const li=document.createElement('li');
-    li.innerHTML=`
-    <span class="task-text">${taskText}</span>
-    <button class="delete-btn"><i class="fa-solid fa-trash" style="color: rgb(87, 144, 172);"></i></button>
+//     const li=document.createElement('li');
+//     li.innerHTML=`
+//     <span class="task-text">${taskText}</span>
+//     <button class="delete-btn">x</button>
+//     `;
+//     taskList.appendChild(li);
+//     taskInput.value="";
+// });
+
+// taskList.addEventListener('click', (e)=>{
+    // if(e.target.classList.contains('delete-btn')){
+
+    // }e.target.parentElemnt.remove();
+
+// });
+// e.target.perentEement.style.backgroudColor="red"
+// if(e.target.classList.contains(task-text)){
+//     e.target.classList.toggle('completed');
+
+// document.querySelectorAll('ul li span.task-text').forEach(span=>{
+//     span.classList.toggle('comleted');
+// });
+
+
+
+const addTask = document.querySelector('#addTask');
+const taskInput = document.querySelector('#taskInput');
+const taskList = document.querySelector('#taskList');
+
+addTask.addEventListener('click', () => {
+    const taskText = taskInput.value.trim();
+    if (!taskText) return alert('please Enter task');
+
+    const li = document.createElement('li');
+    li.innerHTML = `
+        <span class="task-text">${taskText}</span>
+        <button class="delete-btn">x</button>
     `;
     taskList.appendChild(li);
-    taskInput.value="";
+    taskInput.value = "";
+});
 
+taskList.addEventListener('click', (e) => {
 
-})
+    if(e.target.classList.contains('delete-btn')){
+    }
+    e.target.parentElement.remove();
+    e.target.parentElement.style.backgroundColor = "red";
+
+    // if(e.target.classList.contains('task-text')){
+    //     e.target.classList.toggle('completed');
+    document.querySelectorAll('ul li span.task-text').forEach(span => {
+        span.classList.toggle('completed');
+    });
+}); 
