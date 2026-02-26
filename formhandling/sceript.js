f77533e95a2c4d9843722828ff9b8c28d2e07255
Nafisa Ebrahimi <nafisai2750@gmail.com>
2026-02-26 09:14:59 +0430

    add data in submit form

--- formhandling/sceript.js
+++ formhandling/sceript.js
@@ -1,14 +1,24 @@
-const  addField=document.querySelector('#addField');
-const contanier=document.querySelector('#fieldContanier')
-// const submirForm=document.querySelector('#submirForm');
- let count=0;
- addField.addEventListener('click' ,()=>{
+
+const addField = document.querySelector('#addField');
+const contanier = document.querySelector('#fieldContanier');
+const submitBtn = document.querySelector('#submirForm');
+
+let count = 0; 
+
+addField.addEventListener('click', () => {
     count++;
-    const input=document.createElement('input');
-    input.type="text";
-    input.placeholder=`field ${count}`;
-    input.className="form-field";
 
-     contanier.appendChild(input);
+    const input = document.createElement('input');
+    input.type = "text";
+    input.placeholder = `field ${count}`;
+    input.className = "form-field";
 
- })
+    contanier.appendChild(input);
+});
+
+submitBtn.addEventListener('click', () => {
+    const values = [...document.querySelectorAll('.form-field')]
+        .map((field) => field.value);
+
+    console.log("Form Data:", values);
+});
