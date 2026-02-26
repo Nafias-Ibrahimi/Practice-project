a094ff73c6ae5e0b6f36e8873a45ee64c1be8de4
Nafisa Ebrahimi <nafisai2750@gmail.com>
2026-02-26 10:16:36 +0430

    data fetch

--- formhandling/sceript.js
+++ formhandling/sceript.js
@@ -1,39 +1,50 @@
 
-const addField = document.querySelector('#addField');
-const contanier = document.querySelector('#fieldContanier');
-const submitBtn = document.querySelector('#submirForm');
+// const addField = document.querySelector('#addField');
+// const contanier = document.querySelector('#fieldContanier');
+// const submitBtn = document.querySelector('#submirForm');
 
-let count = 0; 
+// let count = 0; 
 
-addField.addEventListener('click', () => {
-    count++;
+// addField.addEventListener('click', () => {
+//     count++;
 
-    const input = document.createElement('input');
-    input.type = "text";
-    input.placeholder = `field ${count}`;
-    input.className = "form-field";
+//     const input = document.createElement('input');
+//     input.type = "text";
+//     input.placeholder = `field ${count}`;
+//     input.className = "form-field";
 
-    contanier.appendChild(input);
-});
-
-// submitBtn.addEventListener('click', () => {
-//     const values = [...document.querySelectorAll('.form-field')]
-//         .map((field) => field.value);
-
-//     console.log("Form Data:", values);
+//     contanier.appendChild(input);
 // });
-submitBtn.addEventListener('click', () => {
-    const fields = document.querySelectorAll('.form-field');
-    let valid = true;
 
-    fields.forEach((field) => {
-        if (field.value.trim() === "") {
-            field.style.border = "2px solid red";
-            valid = false;
-        } else {
-            field.style.border = "2px solid green";
-        }
-    });
+// // submitBtn.addEventListener('click', () => {
+// //     const values = [...document.querySelectorAll('.form-field')]
+// //         .map((field) => field.value);
 
-    if (valid) alert("All fields valid!");
-});
+// //     console.log("Form Data:", values);
+// // });
+// submitBtn.addEventListener('click', () => {
+//     const fields = document.querySelectorAll('.form-field');
+//     let valid = true;
+
+//     fields.forEach((field) => {
+//         if (field.value.trim() === "") {
+//             field.style.border = "2px solid red";
+//             valid = false;
+//         } else {
+//             field.style.border = "2px solid green";
+//         }
+//     });
+
+//     if (valid) alert("All fields valid!");
+// });
+
+// basic Fetch Exmaple
+
+document.getElementById('fetchBtn').addEventListener('click' ,()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then((response)=>response.json())
+    .then((data)=> console.log("Fetch Post:",data))
+    .then((data)=> document.writeln("Fetch Post:",data))
+    .catch((err)=>console.log("Error:" ,err))
+
+})
