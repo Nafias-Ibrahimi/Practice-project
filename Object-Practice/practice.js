a5efe294eed00df204a1ad6f01a7ccd1fe4ad9e9
Nafisa Ebrahimi <nafisai2750@gmail.com>
2026-03-01 09:54:47 +0430

    more examples

--- Object-Practice/practice.js
+++ Object-Practice/practice.js
@@ -7,3 +7,34 @@ console.log(user.name);
 console.log(user["age"]);
 
 console.log(`name: ${user.name}, "age:" ${user.age},"email:" ${user.email}`);
+
+let cars={
+    brand:"Totyota",
+    model:"camry",
+    year:2020
+};
+
+console.log(cars);
+
+
+
+let {brand, model}=cars;
+console.log(`"brand:" ${cars.brand} , "model: ${cars.model}`);
+
+
+let persons={
+    name:"Maryam",
+    age:24,
+    isMarried:false,
+    hobbies:["reading" , "watching" ,"traveling"],
+    address:{city:"kabul", country:"Afghanistan"}
+
+};
+console.log(`"name:" ${persons.name} , "age:" ${persons.age} , "isMarried :" ${persons.isMarried}`);
+
+let {hobbies , address}=persons;
+console.log(persons.hobbies ,persons.address.country);
+
+
+
+
